353383abb39d34497f5b50cadc577fafc486f663
Jadhav-Nandini <nandinij780@gmail.com>
2025-04-26 03:08:44 +0530

    modify home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,15 @@ import Link from "next/link"
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen bg-blue-600/15 flex items-center justify-center px-6 ">
-      <div className="max-w-screen bg-white m-3 sm:m-0 rounded-2xl p-10
-      shadow-[2px_2px_3px_pink,-2px_-3px_3px_] 
-      
-      ">
-        <h1 className="sm:text-4xl text-2xl font-bold ">
+    <div className="h-screen w-screen bg-slate-100 flex items-center justify-center px-6 ">
+      <div className="max-w-screen bg-white rounded-2xl sm:p-10 p-7 shadow-[2px_2px_5px_gray,-1px_-1px_5px_gray]  ">
+        <h1 className="sm:text-4xl text-3xl font-bold  text-slate-800 mb-5 tracking-tight">
           Welcome to the Dashboard App
         </h1>
-        <p>
+        <p className="text-sm text-slate-600 mb-6 leading-relaxed">
           Manage your tasks, track analytics, and stay organized with a clean and inituitive dashboard experience
         </p>
-        <Link href="/dashboard" className=" bg-blue-300 p-2 ">
+        <Link href="/dashboard" className=" bg-indigo-500 text-white px-5 py-3 rounded-xl shadow-md hover:bg-indigo-700 transition duration-400 font-medium ">
         Go to Dashboard
         </Link>
       </div>
@@ -23,9 +20,3 @@ const Home = () => {
 }
 
 export default Home
-
-
-
-
-
-
